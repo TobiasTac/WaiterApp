226fcd34ee3d20db7f66f28b06e6e3909c39719b
TobiasTac <tobias.vida@live.com>
2026-02-20 11:32:30 -0300

    feat: create category service

--- src/router.ts
+++ src/router.ts
@@ -1,3 +1,4 @@
+import { createCategory } from 'app/useCases/categories/createCategory';
 import { Router } from 'express';
 import { listCategories } from './app/useCases/categories/listCategories';
 
@@ -5,3 +6,6 @@ export const router = Router();
 
 // List categories
 router.get('/categories', listCategories);
+
+// Create category
+router.post('/categories', createCategory);
