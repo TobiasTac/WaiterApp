50f6837acdacdbf0a9812b8b252d886a88e5d154
TobiasTac <tobias.vida@live.com>
2026-03-04 18:35:32 -0300

    feat: list orders service

--- api/src/router.ts
+++ api/src/router.ts
@@ -4,6 +4,7 @@ import path from 'node:path';
 import { createCategory } from './app/useCases/categories/createCategory';
 import { listCategories } from './app/useCases/categories/listCategories';
 import { listProductsByCategory } from './app/useCases/categories/listProductsByCategory';
+import { listOrders } from './app/useCases/orders/listOrders';
 import { createProduct } from './app/useCases/products/createProduct';
 import { listProducts } from './app/useCases/products/listProducts';
 
@@ -36,9 +37,7 @@ router.post('/products', upload.single('image'), createProduct);
 router.get('/categories/:categoryId/products', listProductsByCategory);
 
 // List orders
-router.get('/orders', (req, res) => {
-  res.send('OK');
-});
+router.get('/orders', listOrders);
 
 // Create order
 router.post('/orders', (req, res) => {
