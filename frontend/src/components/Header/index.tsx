f32d549bbbbd47445f2a7dc0cbe2e99ab3e80151
TobiasTac <tobias.vida@live.com>
2026-03-04 21:43:36 -0300

    feat: create header component

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import logo from '../assets/images/logo.svg'
+
+export function Header() {
+  return (
+    <header>
+      <div className="page-details">
+        <h1> Pedidos </h1>
+        <h2> Acompanhe os pedidos dos clientes </h2>
+        <img src={logo} alt="WAITERAPP" />
+      </div>
+    </header>
+  )
+}
