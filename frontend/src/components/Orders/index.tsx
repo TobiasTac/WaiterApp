92cd2466b7712431b969c5c540de0fa34abc31ca
TobiasTac <tobias.vida@live.com>
2026-03-05 18:24:44 -0300

    feat: passing title and icon props

--- frontend/src/components/Orders/index.tsx
+++ frontend/src/components/Orders/index.tsx
@@ -4,7 +4,9 @@ import { Container } from './styles'
 export function Orders() {
   return (
     <Container>
-      <OrdersBoard />
+      <OrdersBoard icon="🕑" title="Fila de espera" />
+      <OrdersBoard icon="👩‍🍳" title="Em produção" />
+      <OrdersBoard icon="✅" title="Pronto!" />
     </Container>
   )
 }
--- frontend/src/components/OrdersBoard/index.tsx
+++ frontend/src/components/OrdersBoard/index.tsx
@@ -1,13 +1,29 @@
-import { Board } from './styles'
+import { Board, OrdersContainer } from './styles'
 
-export function OrdersBoard() {
+interface OrdersBoardProps {
+  icon: string
+  title: string
+}
+
+export function OrdersBoard({ icon, title }: OrdersBoardProps) {
   return (
     <Board>
       <header>
-        <span>🕜</span>
-        <strong>Fila de Espera</strong>
+        <span>{icon}</span>
+        <strong>{title}</strong>
         <span>(1)</span>
       </header>
+
+      <OrdersContainer>
+        <button type="button">
+          <strong>Mesa 2</strong>
+          <span>2 itens</span>
+        </button>
+        <button type="button">
+          <strong>Mesa 2</strong>
+          <span>2 itens</span>
+        </button>
+      </OrdersContainer>
     </Board>
   )
 }
