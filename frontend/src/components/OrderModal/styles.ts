fe7208d39570305062fc4964a8e6a2a39bd397e7
TobiasTac <tobias.vida@live.com>
2026-03-05 18:58:20 -0300

    feat: order modal body style

--- frontend/src/components/OrderModal/styles.ts
+++ frontend/src/components/OrderModal/styles.ts
@@ -12,3 +12,42 @@ export const Overlay = styled.div`
   background: rgba(0, 0, 0, 0.8);
   backdrop-filter: blur(4.5px);
 ` as any
+
+export const ModalBody = styled.div`
+  width: 480px;
+  padding: 32px;
+  border-radius: 8px;
+  background: #fff;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    strong {
+      font-size: 24px;
+    }
+
+    button {
+      line-height: 0;
+      border: 0;
+      background: transparent;
+    }
+  }
+
+  .status-container {
+    margin-top: 32px;
+
+    small {
+      font-size: 14px;
+      opacity: 0.8;
+    }
+
+    div {
+      margin-top: 8px;
+      display: flex;
+      align-items: center;
+      gap: 8px;
+    }
+  }
+` as any
