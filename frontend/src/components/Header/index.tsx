edb467d0fe1199c3dbfdcff5155c9c6770e6df1a
TobiasTac <tobias.vida@live.com>
2026-03-04 21:49:32 -0300

    feat: header style

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,13 +1,17 @@
-import logo from '../assets/images/logo.svg'
+import logo from '../../assets/images/logo.svg'
+import { Container, Content } from './styles'
 
 export function Header() {
   return (
-    <header>
-      <div className="page-details">
-        <h1> Pedidos </h1>
-        <h2> Acompanhe os pedidos dos clientes </h2>
+    <Container>
+      <Content>
+        <div className="page-details">
+          <h1> Pedidos </h1>
+          <h2> Acompanhe os pedidos dos clientes </h2>
+        </div>
+
         <img src={logo} alt="WAITERAPP" />
-      </div>
-    </header>
+      </Content>
+    </Container>
   )
 }
