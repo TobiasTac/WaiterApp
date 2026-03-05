32054b0f5fd072445c0f40b40c702f40355d921b
TobiasTac <tobias.vida@live.com>
2026-03-05 17:29:09 -0300

    feat: create orders board component

---
+++ frontend/src/components/OrdersBoard/index.tsx
@@ -0,0 +1,13 @@
+import { Board } from './styles'
+
+export function OrdersBoard() {
+  return (
+    <Board>
+      <header>
+        <span>🕜</span>
+        <strong>Fila de Espera</strong>
+        <span>(1)</span>
+      </header>
+    </Board>
+  )
+}
