cc1a75fc91a9ada67c8c507a297b2a457e2d59e1
TobiasTac <tobias.vida@live.com>
2026-03-06 19:07:56 -0300

    mobile initial commit

--- frontend/src/components/Orders/index.tsx
+++ frontend/src/components/Orders/index.tsx
@@ -1,10 +1,30 @@
+import { Order } from '../../types/Order'
 import { OrdersBoard } from '../OrdersBoard'
 import { Container } from './styles'
+
+const orders: Order[] = [
+  {
+    _id: '69a8a8536b79540b7c8ccdf2',
+    table: '2',
+    status: 'WAITING',
+    products: [
+      {
+        product: {
+          name: 'Pizza',
+          imagePath: '1772580663203-quatro-queijos.png',
+          price: 40,
+        },
+        quantity: 3,
+        _id: '69a8a8536b79540b7c8ccdf3',
+      },
+    ],
+  },
+]
 
 export function Orders() {
   return (
     <Container>
-      <OrdersBoard icon="🕑" title="Fila de espera" orders={[]} />
+      <OrdersBoard icon="🕑" title="Fila de espera" orders={orders} />
       <OrdersBoard icon="👩‍🍳" title="Em produção" orders={[]} />
       <OrdersBoard icon="✅" title="Pronto!" orders={[]} />
     </Container>
