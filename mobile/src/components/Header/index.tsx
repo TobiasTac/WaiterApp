4febd6c8e30be6a786d1aff394460081b924e21b
TobiasTac <tobias.vida@live.com>
2026-03-07 16:23:40 -0300

    fix: header text weight prop name was wrong

--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -7,7 +7,7 @@ export function Header() {
       <Text size={14} opacity={0.9}>
         Bem vindo (a) ao
       </Text>
-      <Text size={24} wight="700">
+      <Text size={24} weight="700">
         WAITER
         <Text size={24}>APP</Text>
       </Text>
