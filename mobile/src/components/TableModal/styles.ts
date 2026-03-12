0248534b4843e9cb3bb8309ecbf0246a1cf55a25
TobiasTac <tobias.vida@live.com>
2026-03-12 11:35:00 -0300

    feat: overlay style

---
+++ mobile/src/components/TableModal/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native'
+
+export const Overlay = styled.View`
+  background: rgba(0, 0, 0, 0.6);
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`
