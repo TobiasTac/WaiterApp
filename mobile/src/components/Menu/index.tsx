b894a54304234aae5ee7291d792443e008ad1b9f
TobiasTac <tobias.vida@live.com>
2026-03-12 09:58:59 -0300

    feat: add to card button

--- mobile/src/components/Menu/index.tsx
+++ mobile/src/components/Menu/index.tsx
@@ -2,8 +2,15 @@ import React from 'react'
 import { FlatList } from 'react-native'
 import { products } from '../../mocks/products'
 import { formatCurrency } from '../../utils/formatCurrency'
+import { PlusCircle } from '../Icons/PlusCircle'
 import { Text } from '../Text'
-import { Product, ProductDetails, ProductImage, Separator } from './styles'
+import {
+  AddToCartButton,
+  Product,
+  ProductDetails,
+  ProductImage,
+  Separator
+} from './styles'
 
 export function Menu() {
   return (
@@ -29,6 +36,10 @@ export function Menu() {
               {formatCurrency(product.price)}
             </Text>
           </ProductDetails>
+
+          <AddToCartButton>
+            <PlusCircle />
+          </AddToCartButton>
         </Product>
       )}
     />
