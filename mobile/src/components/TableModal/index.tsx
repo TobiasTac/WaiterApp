a886781476d0e13416fcacf4fdc8b3ff871c45cf
TobiasTac <tobias.vida@live.com>
2026-03-12 20:19:55 -0300

    feat: get table number of the modal

--- mobile/src/components/TableModal/index.tsx
+++ mobile/src/components/TableModal/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Modal, Platform, TouchableOpacity } from 'react-native'
 import { Button } from '../Button'
 import { Close } from '../Icons/Close'
@@ -6,17 +7,20 @@ import { Form, Header, Input, ModalBody, Overlay } from './styles'
 
 interface TableModalProps {
   visible: boolean
+  onClose(): void
 }
 
-export function TableModal({ visible }: TableModalProps) {
+export function TableModal({ visible, onClose }: TableModalProps) {
+  const [table, setTable] = useState('')
+
   return (
-    <Modal transparent visible={visible}>
+    <Modal transparent visible={visible} animationType="fade">
       <Overlay behavior={Platform.OS === 'android' ? 'height' : 'padding'}>
         <ModalBody>
           <Header>
             <Text height="600">Informe a mesa</Text>
 
-            <TouchableOpacity>
+            <TouchableOpacity onPress={onClose}>
               <Close color="#666" />
             </TouchableOpacity>
           </Header>
@@ -26,9 +30,12 @@ export function TableModal({ visible }: TableModalProps) {
               placeholder="Número da mesa"
               placeholderTextColor="#666"
               keyboardType="number-pad"
+              onChangeText={setTable}
             />
 
-            <Button onPress={() => alert('Salvou')}> Salvar </Button>
+            <Button onPress={() => alert(table)} disabled={table.length === 0}>
+              Salvar
+            </Button>
           </Form>
         </ModalBody>
       </Overlay>
