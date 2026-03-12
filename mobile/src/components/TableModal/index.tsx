68569ac37d85ad2bfa98c29815891924db2d076c
TobiasTac <tobias.vida@live.com>
2026-03-12 11:37:32 -0300

    feat: modal body style

--- mobile/src/components/TableModal/index.tsx
+++ mobile/src/components/TableModal/index.tsx
@@ -1,10 +1,15 @@
 import { Modal } from 'react-native'
 import { Text } from '../Text'
+import { ModalBody, Overlay } from './styles'
 
 export function TableModal() {
   return (
     <Modal transparent>
-      <Text>Hello</Text>
+      <Overlay>
+        <ModalBody>
+          <Text Text>Hello</Text>
+        </ModalBody>
+      </Overlay>
     </Modal>
   )
 }
--- mobile/src/components/TableModal/styles.ts
+++ mobile/src/components/TableModal/styles.ts
@@ -3,6 +3,13 @@ import styled from 'styled-components/native'
 export const Overlay = styled.View`
   background: rgba(0, 0, 0, 0.6);
   flex: 1;
-  align-items: center;
+  align-items: stretch;
   justify-content: center;
+  padding: 0 24px;
+`
+
+export const ModalBody = styled.View`
+  background: #fafafa;
+  border-radius: 8px;
+  padding: 24px;
 `
