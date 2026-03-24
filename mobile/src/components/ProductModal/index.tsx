fa9e2f2274818d0d00336f8b210851317b013c1f
TobiasTac <tobias.vida@live.com>
2026-03-24 10:35:06 -0300

    feat: product modal close button

--- mobile/src/components/ProductModal/index.tsx
+++ mobile/src/components/ProductModal/index.tsx
@@ -1,7 +1,8 @@
 import { Modal } from 'react-native'
 import { Product } from '../../types/Product'
+import { Close } from '../Icons/Close'
 import { Text } from '../Text'
-import { Image } from './styles'
+import { CloseButton, Image } from './styles'
 
 interface ProductModalProps {
   visible: boolean
@@ -26,7 +27,11 @@ export function ProductModal({ visible, onClose, product }: ProductModalProps) {
           uri: `http://10.10.254.67:3001/uploads/${product.imagePath}`
           //uri: `http://192.168.0.18:3001/uploads/${product?.imagePath}`
         }}
-      ></Image>
+      >
+        <CloseButton onPress={onClose}>
+          <Close />
+        </CloseButton>
+      </Image>
       <Text> Fala rapeize </Text>
     </Modal>
   )
