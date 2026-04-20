755dff7893748ef60586deb1df34d77c9c173ae3
TobiasTac <tobias.vida@live.com>
2026-04-20 18:49:12 -0300

    feat: conditional rendering for an empty cart

--- mobile/src/components/Cart/index.tsx
+++ mobile/src/components/Cart/index.tsx
@@ -25,57 +25,68 @@ interface CartProps {
 export function Cart({ cartItems }: CartProps) {
   return (
     <>
-      <FlatList
-        data={cartItems}
-        keyExtractor={cartItem => cartItem.product._id}
-        showsVerticalScrollIndicator={false}
-        style={{ marginBottom: 20, maxHeight: 150 }}
-        renderItem={({ item: cartItem }) => (
-          <Item>
-            <ProductContainer>
-              <Image
-                source={{
-                  uri: `http://10.10.240.239:3001/uploads/${cartItem.product.imagePath}`
-                  //uri: `http://192.168.0.18:3001/uploads/${product?.imagePath}`
-                }}
-              />
+      {cartItems.length > 0 && (
+        <FlatList
+          data={cartItems}
+          keyExtractor={cartItem => cartItem.product._id}
+          showsVerticalScrollIndicator={false}
+          style={{ marginBottom: 20, maxHeight: 150 }}
+          renderItem={({ item: cartItem }) => (
+            <Item>
+              <ProductContainer>
+                <Image
+                  source={{
+                    // uri: `http://10.10.240.239:3001/uploads/${cartItem.product.imagePath}`
+                    uri: `http://192.168.0.18:3001/uploads/${cartItem.product.imagePath}`
+                  }}
+                />
 
-              <QuantityContainer>
-                <Text size={14} color="#666">
-                  {cartItem.quantity} x
-                </Text>
-              </QuantityContainer>
+                <QuantityContainer>
+                  <Text size={14} color="#666">
+                    {cartItem.quantity} x
+                  </Text>
+                </QuantityContainer>
 
-              <ProductDetails>
-                <Text size={14} weight="600">
-                  {cartItem.product.name}
-                </Text>
-                <Text size={14} color="#666" style={{ marginTop: 4 }}>
-                  {formatCurrency(cartItem.product.price)}
-                </Text>
-              </ProductDetails>
-            </ProductContainer>
-            <Actions>
-              <TouchableOpacity style={{ marginRight: 24 }}>
-                <PlusCircle />
-              </TouchableOpacity>
+                <ProductDetails>
+                  <Text size={14} weight="600">
+                    {cartItem.product.name}
+                  </Text>
+                  <Text size={14} color="#666" style={{ marginTop: 4 }}>
+                    {formatCurrency(cartItem.product.price)}
+                  </Text>
+                </ProductDetails>
+              </ProductContainer>
+              <Actions>
+                <TouchableOpacity style={{ marginRight: 24 }}>
+                  <PlusCircle />
+                </TouchableOpacity>
 
-              <TouchableOpacity>
-                <MinusCircle />
-              </TouchableOpacity>
-            </Actions>
-          </Item>
-        )}
-      />
+                <TouchableOpacity>
+                  <MinusCircle />
+                </TouchableOpacity>
+              </Actions>
+            </Item>
+          )}
+        />
+      )}
 
       <Summary>
         <TotalContainer>
-          <Text color="#666"> Total </Text>
-          <Text size={20} weight="600">
-            {formatCurrency(120)}
-          </Text>
+          {cartItems.length > 0 ? (
+            <>
+              <Text color="#666"> Total </Text>
+              <Text size={20} weight="600">
+                {formatCurrency(120)}
+              </Text>
+            </>
+          ) : (
+            <Text color="#999"> Seu carrinho está vazio </Text>
+          )}
         </TotalContainer>
-        <Button onPress={() => alert('Confirmar Pedido')}>
+        <Button
+          onPress={() => alert('Confirmar Pedido')}
+          disabled={cartItems.length === 0}
+        >
           Confirmar Pedido
         </Button>
       </Summary>
