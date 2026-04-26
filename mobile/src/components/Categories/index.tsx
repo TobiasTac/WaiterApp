485fd32526b8bedc6e2e0122ee92d34bf6e00441
TobiasTac <tobias.vida@live.com>
2026-04-26 19:42:07 -0300

    feat: implements categories props

--- mobile/src/components/Categories/index.tsx
+++ mobile/src/components/Categories/index.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import { FlatList } from 'react-native'
-import { categories } from '../../mocks/categories'
+import { type Category } from '../../types/Category'
 import { Text } from '../Text'
-import { Category, Icon } from './styles'
+import { CategoryContainer, Icon } from './styles'
 
-export function Categories() {
+interface CategoriesProps {
+  categories: Category[]
+}
+
+export function Categories({ categories }: CategoriesProps) {
   const [selectedCategory, setSelectedCategory] = useState('')
 
   function handleSelectCategory(CategoryId: string) {
@@ -23,7 +27,7 @@ export function Categories() {
       renderItem={({ item: category }) => {
         const isSelected = selectedCategory === category._id
         return (
-          <Category onPress={() => handleSelectCategory(category._id)}>
+          <CategoryContainer onPress={() => handleSelectCategory(category._id)}>
             <Icon>
               <Text opacity={isSelected ? 1 : 0.5}>{category.icon}</Text>
             </Icon>
@@ -31,7 +35,7 @@ export function Categories() {
             <Text size={14} weight="600" opacity={isSelected ? 1 : 0.5}>
               {category.name}
             </Text>
-          </Category>
+          </CategoryContainer>
         )
       }}
     />
