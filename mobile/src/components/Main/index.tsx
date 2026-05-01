e5b9c5dd98151c2361594e81b8d19afff8705aa9
TobiasTac <tobias.vida@live.com>
2026-05-01 12:23:02 -0300

    feat: implements is loading products

--- mobile/src/components/Main/index.tsx
+++ mobile/src/components/Main/index.tsx
@@ -27,6 +27,7 @@ export function Main() {
   const [isLoading, setIsLoading] = useState(true)
   const [products, setProducts] = useState<Product[]>([])
   const [categories, setCategories] = useState<Category[]>([])
+  const [isLoadingProducts, setIsLoadingProducts] = useState(false)
 
   useEffect(() => {
     Promise.all([api.get('categories'), api.get('products')]).then(
@@ -43,8 +44,11 @@ export function Main() {
       ? '/products'
       : `/categories/${categoryId}/products`
 
+    setIsLoadingProducts(true)
+
     const { data } = await api.get(route)
     setProducts(data)
+    setIsLoadingProducts(false)
   }
 
   function handleSaveTable(table: string) {
@@ -129,17 +133,25 @@ export function Main() {
               />
             </CategoriesContainer>
 
-            {products.length > 0 ? (
-              <MenuContainer>
-                <Menu onAddToCart={handleAddToCart} products={products} />
-              </MenuContainer>
-            ) : (
+            {isLoadingProducts ? (
               <CenteredContainer>
-                <Empty />
-                <Text color="#666" style={{ marginTop: 24 }}>
-                  Nenhum produto foi encontrado!
-                </Text>
+                <ActivityIndicator color="#D73035" size="large" />
               </CenteredContainer>
+            ) : (
+              <>
+                {products.length > 0 ? (
+                  <MenuContainer>
+                    <Menu onAddToCart={handleAddToCart} products={products} />
+                  </MenuContainer>
+                ) : (
+                  <CenteredContainer>
+                    <Empty />
+                    <Text color="#666" style={{ marginTop: 24 }}>
+                      Nenhum produto foi encontrado!
+                    </Text>
+                  </CenteredContainer>
+                )}
+              </>
             )}
           </>
         )}
