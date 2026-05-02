3eb23ec26f4959e5fca591edecd93489c09100b3
TobiasTac <tobias.vida@live.com>
2026-05-02 18:38:09 -0300

    feat(front): separating order by status

--- frontend/src/components/Orders/index.tsx
+++ frontend/src/components/Orders/index.tsx
@@ -13,11 +13,17 @@ export function Orders() {
     })
   }, [])
 
+  const waiting = orders.filter((order) => order.status === 'WAITING')
+  const inProduction = orders.filter(
+    (order) => order.status === 'IN_PRODUCTION',
+  )
+  const done = orders.filter((order) => order.status === 'DONE')
+
   return (
     <Container>
-      <OrdersBoard icon="🕑" title="Fila de espera" orders={orders} />
-      <OrdersBoard icon="👩‍🍳" title="Em produção" orders={orders} />
-      <OrdersBoard icon="✅" title="Pronto!" orders={orders} />
+      <OrdersBoard icon="🕑" title="Fila de espera" orders={waiting} />
+      <OrdersBoard icon="👩‍🍳" title="Em produção" orders={inProduction} />
+      <OrdersBoard icon="✅" title="Pronto!" orders={done} />
     </Container>
   )
 }
